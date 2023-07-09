3fc83f2db386a10627efc3883606f7bfcaac502c
Jacob Broughton <jlbroughton88@gmail.com>
2023-07-09 14:30:15 -0400

    added leetcode link to social icons

--- components/SocialIcons.js
+++ components/SocialIcons.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import SVGIcon from "../components/SVGIcon"
-import socialIconsStyles from "../styles/SocialIcons.module.scss"
+import React from "react";
+import SVGIcon from "../components/SVGIcon";
+import socialIconsStyles from "../styles/SocialIcons.module.scss";
 
 const SocialIcons = () => {
+  const links = [
+    {
+      icon: "linkedIn",
+      link: "https://www.linkedin.com/in/jacob-broughton-637189164/",
+    },
+    {
+      icon: "github",
+      link: "https://www.github.com/jacobbroughton",
+    },
+    {
+      icon: "leetcode",
+      link: "https://leetcode.com/jlbroughton/",
+    },
+    {
+      icon: "youtube",
+      link: "https://www.youtube.com/channel/UCnHjcw_ZQwEQAUbXPbUecWA",
+    },
+  ];
+
   return (
     <div className={socialIconsStyles.socialIcons}>
-      <a className={socialIconsStyles.socialLink} href="https://www.linkedin.com/in/jacob-broughton-637189164/">
-        <SVGIcon type="linkedIn" />
-      </a>
-      <a className={socialIconsStyles.socialLink} href="https://www.github.com/jacobbroughton">
-        <SVGIcon type="github" />
-      </a>
-      <a className={socialIconsStyles.socialLink} href="https://www.youtube.com/channel/UCnHjcw_ZQwEQAUbXPbUecWA">
-        <SVGIcon type="youtube" />
-      </a>
+      {links.map((link) => (
+        <a key={link.icon} className={socialIconsStyles.socialLink} href={link.link}>
+          <SVGIcon type={link.icon} />
+        </a>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default SocialIcons
+export default SocialIcons;
